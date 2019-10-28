d3e13af93cc5a9a5162a5ae02613146bf96f1b3a
Lawton Mizell <alcamech@gmail.com>
2019-10-28 09:24:41 -0400

    added test for lexrank implementation

--- test/clj_lexrank/core_test.clj
+++ test/clj_lexrank/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [clj-lexrank.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest lexrank-test
+  (testing "Testing lexrank implementation."
+    (is (=
+          (lexrank "test-documents/test4.txt" 0.2 0.1 3 true)
+          '("In this blog post, we will walk you through the different steps that are necessary to get you started with Docker Compose and show you how to use it."
+             "To demonstrate the benefits of Docker Compose, we will create a simple Node.js \"Hello World\" application, which will run on 3 Docker Node.js containers."
+             "HTTP requests will be distributed to these Node.js")))))
+
+
